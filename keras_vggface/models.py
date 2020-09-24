53f97770ea8b94835f8a69b8c37fec191a2e760f
Nils Lukas <nlukas@uwaterloo.ca>
2020-09-24 19:37:05 -0400

    change to tf keras

--- keras_vggface/models.py
+++ keras_vggface/models.py
@@ -15,12 +15,12 @@ from tensorflow.keras.layers import Flatten, Dense, Input, GlobalAveragePooling2
 from keras_applications.imagenet_utils import _obtain_input_shape
 from tensorflow.keras.utils import layer_utils
 from tensorflow.keras.utils.data_utils import get_file
-from keras import backend as K
+from tensorflow.keras import backend as K
+from tensorflow.keras import layers
 from keras_vggface import utils
 from tensorflow.keras.engine.topology import get_source_inputs
 import warnings
 from tensorflow.keras.models import Model
-from keras import layers
 
 
 def VGG16(include_top=True, weights='vggface',
